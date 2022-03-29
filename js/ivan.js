c026985a4cc156028a32f6200d4932b7d90805b3
Timur Tsoi <timurchoi2010@gmail.com>
2022-03-29 10:56:59 -0500

    my commit to girhub

---
+++ js/ivan.js
@@ -0,0 +1,111 @@
+// console.log("hello world");
+// alert(" what you name");
+// const result = confirm("what you real name?");
+// console.log(result);
+// const answer = +prompt (" how old are you?", "");
+// console.log(answer +  "you dont live one");
+/*const answers =[];
+answers[0] = prompt (" what you name?",  "");
+answers[1] = prompt (" how are old you?",  "");
+answers[2] = prompt (" what you last name?",  "");
+answers[3] = prompt (" are you marrige?",  "");
+document.write(answers);*/
+// console.log(4 + + "5");
+// let incr = 10,
+//     decr = 10;
+//     incr ++;
+//     decr --;
+//     console.log(incr);
+//     console.log(decr);
+// const numberOfFilms = +promt ("how much moves did you see", "");
+let numberOfFilms = +promt( "Сколько фильмов вы уже посмотрели?", "");
+// const num = 100;
+// if ( num == 50){
+//     console.log("no");
+
+// }else if ( num = 103){
+
+
+// console.log( "good");}
+// else{console.log ( ok);
+// }
+// (num===100) ? console.log("ok") : console.log ("no");
+// if(4==4){
+//     console.log( "error");
+// }else{
+//     console.log("ok");
+// }
+            // const num = 50;
+            // if (num<49){
+            //     console.log( "error");
+            // }else if ( num > 100){
+            //     console.log("ok");
+            // }else{ console.log ('ok');
+            // }
+            // (num ===50) ? console.log ("ok") : console.log ("error");
+
+
+// if ( num<28){
+//     console.log("wrong");
+// } else if ( num>30){
+//         console.log ("too");}
+//     else if ( num <=31){
+//     console.log(" done");
+// }
+// else {
+//     console.log(" uuurrra");
+// }
+// (num==31)  ? console.log ("ok") :  console.log(" error") ;
+// 
+
+// if ( num >22){
+//     console.log (" error");
+// }else if (num < 12){
+//     console.log(" again");
+//     }else console.log(" uuura");
+//     (num ==20) ? console.log ("ok") : console.log ("error");
+// ( num==30) ? console.log("ok") :console.log ("error");
+// var num = 20;
+// switch ( num ){
+// case 19:
+//     console.log ( "неверно" );
+//     break ;
+// case 190:
+//     console.log ( "неверно" );
+//     break ;
+
+// case 14 :
+//     console.log ( "неверно" );
+//     break ;
+
+// case 15:
+//     console.log ( "неверно" );
+//     break ;
+//     case 20:
+//         console.log (" ok ");
+//         break;
+// default:
+//     console.log(" try again " );
+//     break;
+// }
+" use strict";
+let num = 50;
+while (num < 55) {
+    console.log(num);
+    num++;
+}
+do{
+    console.log(num);
+    num++;
+}
+while(num<55)
+promt ( "how are you");
+alert("hell");
+
+let q = 0;
+while (q < 5){
+    console.log(q);
+    q++;
+} 
+
+    
